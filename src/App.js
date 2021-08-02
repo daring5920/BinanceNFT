f6a3be6bf2bcdb3095d01aa2f660e552cc0ac7f0
MicFenne <fennemic@gmail.com>
2021-08-02 11:48:50 +0200

    Enter the price in USD; Clearer data; Added links to NFTs

--- src/App.js
+++ src/App.js
@@ -1,68 +1,191 @@
 import React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
+import Link from '@material-ui/core/Link';
+import axios from 'axios';
 
-const columns = [
-  {
-    field: "id",
-    type: "number",
-    headerName: "ID",
-    width: 100,
-    editable: false
-  },
-  {
-    field: "amount",
-    type: "number",
-    headerName: "Prezzo",
-    width: 150,
-    editable: false
-  },
-  {
-    field: "currency",
-    headerName: "Valuta",
-    width: 150,
-    editable: false
-  },
-  {
-    field: "title",
-    headerName: "Nome",
-    type: "number",
-    width: 250,
-    editable: false
-  },
-  {
-    field: "tradeType",
-    headerName: "Asta",
-    type: "number",
-    width: 150,
-    editable: false
-  }
-];
+export default function App(data) {
 
-  class App extends React.Component {
-    render() {
-      const rows = this.props.data.items.map((row) => {
-        const { productId, ...rest } = row;
-        return { id: productId, ...rest };
-      });
-
-      if (this.props.data.error) {
-        return <div>Error: {this.props.data.error.message}</div>;
-      } else if (!this.props.data.isLoaded) {
-        return <div>Loading...</div>;
-      } else {
-        return (
-          <div style={{ height: 1000, width: "100%" }}>
-            <DataGrid
-              rows={rows}
-              columns={columns}
-              id="productId"
-              pageSize={100}
-            />
-          </div>
-        );
-      }
+  const columns = [
+    {
+      field: "id",
+      type: "number",
+      headerName: "ID",
+      width: 100,
+      editable: false
+    },
+    {
+      field: "title",
+      headerName: "Name",
+      width: 250,
+      editable: false
+    },
+    {
+      field: "amount",
+      headerName: "Price",
+      type: "number",
+      width: 115,
+      editable: false
+    },
+    {
+      field: "currency",
+      headerName: "Coin",
+      width: 120,
+      editable: false
+    },
+    {
+      field: "usd",
+      headerName: "Price USD",
+      type: "number",
+      width: 150,
+      editable: false
+    },
+    {
+      field: "tradeType",
+      headerName: "Trade Type",
+      type: "number",
+      width: 150,
+      editable: false
+    },
+    {
+      field: "url",
+      headerName: "URL",
+      width: 250,
+      renderCell: (params) => (
+        <Link target="_blank" href={params.value}>{params.value}</Link>),
+      editable: false
     }
+  ];
+
+  /*const newItem = []
+  if (data.data.numItems > 100 ) {
+    let totalItem = data.data.numItems - 100
+    let page = 2
+    let newBody = "{\"category\":0,\"keyword\":\"" + data.data.itemName + "\",\"orderBy\":\"list_time\",\"orderType\":-1,\"page\":" + page + ",\"rows\":100}"
+    do {
+      axios({
+        method: 'post',
+        url: 'https://www.binance.com/bapi/nft/v1/public/nft/product-list',
+        headers: {
+          "accept": "**",
+          "accept-language": "it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7",
+          "clienttype": "web",
+          "content-type": "application/json",
+          "lang": "en"
+        },
+        data: newBody
+      })
+        .then((response) => {
+          newItem.setState.Push({
+            newItem: response.data.data.rows
+          });
+        })
+        .catch((error) => 
+        {
+          //
+        });
+
+        page ++ 
+        totalItem = totalItem -100
+        newBody = "{\"category\":0,\"keyword\":\"" + data.data.itemName + "\",\"orderBy\":\"list_time\",\"orderType\":-1,\"page\":" + page + ",\"rows\":100}"
+    } while (totalItem > 0)
+  }*/
+
+  const rows = data.data.items.map((row) => {
+    const { productId, ...rest } = row;
+    return { id: productId, ...rest };
+  });
+  const coins = [data.data.coins.data]
+
+  const finalRows = [];
+  console.log(finalRows)
+  for (var i = 0; i < rows.length; i++) {
+    if (rows[i].tradeType === 1) {
+      if (rows[i].currency === "BUSD") {
+        finalRows[i] =   
+      {
+        id: rows[i].id,
+        title: rows[i].title,
+        usd:rows[i].amount,
+        amount: rows[i].amount,
+        currency: rows[i].currency,
+        tradeType: "Auction",
+        url: "https://www.binance.com/en/nft/goods/blindBox/detail?productId="+rows[i].id+"&isOpen=true&isProduct=1"
+      }
+      } else if (rows[i].currency === "BNB") {
+        finalRows[i] =   
+        {
+          id: rows[i].id,
+          title: rows[i].title,
+          usd: coins[0].binancecoin.usd * rows[i].amount,
+          amount: rows[i].amount,
+          currency: rows[i].currency,
+          tradeType: "Auction",
+          url: "https://www.binance.com/en/nft/goods/blindBox/detail?productId="+rows[i].id+"&isOpen=true&isProduct=1"
+        }
+        } else if (rows[i].currency === "ETH") {
+          finalRows[i] =   
+          {
+            id: rows[i].id,
+            title: rows[i].title,
+            usd: coins[0].ethereum.usd * rows[i].amount,
+            amount: rows[i].amount,
+            currency: rows[i].currency,
+            tradeType: "Auction",
+            url: "https://www.binance.com/en/nft/goods/blindBox/detail?productId="+rows[i].id+"&isOpen=true&isProduct=1"
+          }
+        }
+    } else {
+      if (rows[i].currency === "BUSD") {
+        finalRows[i] =   
+      {
+        id: rows[i].id,
+        title: rows[i].title,
+        usd:rows[i].amount,
+        amount: rows[i].amount,
+        currency: rows[i].currency,
+        tradeType: "Buy Now",
+        url: "https://www.binance.com/en/nft/goods/blindBox/detail?productId="+rows[i].id+"&isOpen=true&isProduct=1"
+      }
+      } else if (rows[i].currency === "BNB") {
+        finalRows[i] =   
+        {
+          id: rows[i].id,
+          title: rows[i].title,
+          usd: coins[0].binancecoin.usd * rows[i].amount,
+          amount: rows[i].amount,
+          currency: rows[i].currency,
+          tradeType: "Buy Now",
+          url: "https://www.binance.com/en/nft/goods/blindBox/detail?productId="+rows[i].id+"&isOpen=true&isProduct=1"
+        }
+        } else if (rows[i].currency === "ETH") {
+          finalRows[i] =   
+          {
+            id: rows[i].id,
+            title: rows[i].title,
+            usd: coins[0].ethereum.usd * rows[i].amount,
+            amount: rows[i].amount,
+            currency: rows[i].currency,
+            tradeType: "Buy Now",
+            url: "https://www.binance.com/en/nft/goods/blindBox/detail?productId="+rows[i].id+"&isOpen=true&isProduct=1"
+          }
+        }
+    }
+  } 
+
+  if (data.data.error) {
+    return <div>Error: {data.data.error.message}</div>;
+  } else if (!data.data.isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div style={{ height: 1200, width: "100%" }}>
+        <DataGrid
+          rows={finalRows}
+          columns={columns}
+          pageSize={100}
+          density="compact"
+        />
+      </div>
+    );
   }
-
-
-export { App };
+}
--- src/Core.js
+++ src/Core.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
-import { App } from './App';
+import App from './App';
 
 class Core extends React.Component {
   constructor(props) {
@@ -12,10 +12,16 @@ class Core extends React.Component {
       body: "",
       error: null,
       isLoaded: false,
-      items: []
+      items: [],
+      itemName: "",
+      numItems: null,
+      coins: []
     };
+    this.mySubmitHandler = this.mySubmitHandler.bind(this);
+    this.myChangeHandler = this.myChangeHandler.bind(this);
   }
-  mySubmitHandler = (event) => {
+  
+  mySubmitHandler = async (event) => {
     event.preventDefault();
     axios({
       method: 'post',
@@ -32,22 +38,39 @@ class Core extends React.Component {
       .then((response) => {
         this.setState({
           isLoaded: true,
-          items: response.data.data.rows
+          showComponent: true,
+          items: response.data.data.rows,
+          numItems: response.data.data.total
         });
       })
       .catch((error) => 
       {
         this.setState({
           isLoaded: true,
+          showComponent: true,
           error
         });
       });
-
+      axios({
+        method: 'get',
+        url: 'https://api.coingecko.com/api/v3/simple/price?ids=binancecoin%2Cethereum&vs_currencies=usd'
+      })
+        .then((response) => {
+          this.setState({
+            coins: response
+          });
+        })
+        .catch((error) => 
+        {
+          this.setState({
+            error
+          });
+        });
   }
   myChangeHandler = (event) => {
     this.setState({
       body: "{\"category\":0,\"keyword\":\"" + event.target.value + "\",\"orderBy\":\"list_time\",\"orderType\":-1,\"page\":1,\"rows\":100}",
-      showComponent: true
+      itemName: event.target.value
     });
   }
   render() {
@@ -72,6 +95,7 @@ class Core extends React.Component {
           Search
         </Button>
       </form>
+
       {this.state.showComponent ?
       <App data={this.state}/> :
       null
